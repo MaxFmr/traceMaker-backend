51d134f28add11cec74edb03d2035e1d8094d221
Maxime Kerlidou <maxime.kerlidou@gmail.com>
2022-01-19 15:38:06 +0100

    .csv reponse

--- routes/admin.js
+++ routes/admin.js
@@ -85,7 +85,7 @@ router.get("/admin/consult/csv/:_id", async (req, res) => {
     console.log(response.data);
 
     const csv = response.data;
-    res.set("Content-Type", "application/octet-stream");
+    res.set("Content-Type", "text/csv");
     res.send(csv);
   } catch (error) {
     res.status(400).json({ error: error.message });
